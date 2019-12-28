6cacf42599231e3fa3f9785920c0a6b2d6ffeb13
3li3bdalla <ali.dev.sd@gmail.com>
2019-12-28 17:17:17 +0300

    update

---
+++ resources/views/accounting/printer/printers.blade.php
@@ -0,0 +1,5 @@
+@extends("accounting.layout.master")
+
+@section("content")
+    <accounting-printers-setting-layout-component></accounting-printers-setting-layout-component>
+@endsection
---
+++ storage/framework/views/5508b920b33dad43b5f8303c0cbf628f399608a6.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection("content"); ?>
+    <accounting-printers-setting-layout-component></accounting-printers-setting-layout-component>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make("accounting.layout.master", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/vagrant/code/zilrsoft/resources/views/accounting/printer/printers.blade.php ENDPATH**/ ?>
