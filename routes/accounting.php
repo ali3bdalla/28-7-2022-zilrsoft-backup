01b12503d76555d8e9ae7514b73639b7b0a67115
3li3bdalla <ali.dev.sd@gmail.com>
2019-12-28 20:46:46 +0300

    udpate

--- routes/accounting.php
+++ routes/accounting.php
@@ -5,6 +5,12 @@
 	app()->setLocale('ar');
 	Auth::routes(["verify" => true]);
 	
+	
+	Route::name('printer')->prefix('printer')->group(function (){
+		Route::get('print_receipt/{sale}','PrinterController@print_receipt');
+	});
+	
+	
 	Route::get('/','HomeController@index');
 	Route::middleware(['auth','verified'])->group(function (){
 		Route::resources([
@@ -30,7 +36,7 @@
 		Route::name('printer')->prefix('printer')->group(function (){
 			Route::get('sign_receipt_printer','PrinterController@sign_receipt_printer');
 			Route::get('printers','PrinterController@printers');
-			Route::get('print_receipt/{sale}','PrinterController@print_receipt');
+//			Route::get('print_receipt/{sale}','PrinterController@print_receipt');
 		});
 		
 		Route::name('items.')->prefix('items')->group(function (){
