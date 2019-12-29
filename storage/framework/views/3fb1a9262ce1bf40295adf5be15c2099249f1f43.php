7d4ca6fa7f94e65dc64742c74554d62f2098d110
3li3bdalla <ali.dev.sd@gmail.com>
2019-12-29 16:59:31 +0300

    update git

--- storage/framework/views/3fb1a9262ce1bf40295adf5be15c2099249f1f43.php
+++
@@ -1,8 +0,0 @@
-<script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery/dist/jquery.min.js"></script>
-<script src="https://adminlte.io/themes/AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="https://adminlte.io/themes/AdminLTE/dist/js/adminlte.min.js"></script>
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/js/select2.min.js" defer></script>
-<script src="<?php echo e(asset('js/main.js')); ?>" defer></script>
-<?php /**PATH /home/vagrant/code/zilrsoft/resources/views/accounting/layout/head/js.blade.php ENDPATH**/ ?>
