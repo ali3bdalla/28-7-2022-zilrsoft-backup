49a0c7140e6ede766fa96b238fd366123de897f3
3li3bdalla <ali.dev.sd@gmail.com>
2020-01-01 19:07:15 +0300

    update git

--- app/Http/Requests/Accounting/Sale/DatatableRequest.php
+++ app/Http/Requests/Accounting/Sale/DatatableRequest.php
@@ -53,7 +53,7 @@
 			}elseif ($this->has('startDate') && $this->filled('startDate')){
 				$query = $query->whereDate('created_at',Carbon::parse($this->input("startDate")));
 			}else{
-				if (!$this->user()->can('manage branches') && !$this->filled('id'))
+				if (!$this->user()->can('manage branches') && !$this->filled('title'))
 					$query = $query->whereDate('created_at',Carbon::today());
 			}
 			
