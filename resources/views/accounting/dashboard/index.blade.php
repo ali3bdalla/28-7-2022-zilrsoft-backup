955138b49f3aecd7b5104cb821688b9f133d356e
3li3bdalla <ali.dev.sd@gmail.com>
2020-01-05 21:56:26 +0300

    update git

--- resources/views/accounting/dashboard/index.blade.php
+++ resources/views/accounting/dashboard/index.blade.php
@@ -12,51 +12,53 @@
 
 
 @section("content")
-    <div class="row">
-        <div class="col-md-4">
-            <accounting-dashboard-active-items-layout-component>
+    @can(' view charts')
+        <div class="row">
+            <div class="col-md-4">
+                <accounting-dashboard-active-items-layout-component>
 
-            </accounting-dashboard-active-items-layout-component>
+                </accounting-dashboard-active-items-layout-component>
 
+            </div>
+            <div class="col-md-4">
+                <accounting-dashboard-items-chart-component
+                        type="bar"
+                        title="منتجات">
+
+                </accounting-dashboard-items-chart-component>
+            </div>
+            <div class="col-md-4">
+                <accounting-dashboard-items-chart-component
+                        type="radar"
+                        title="فلاتر">
+
+                </accounting-dashboard-items-chart-component>
+            </div>
         </div>
-        <div class="col-md-4">
-            <accounting-dashboard-items-chart-component
-                    type="bar"
-                    title="منتجات">
+        {{--    <div class="row">--}}
+        {{--        <div class="col-md-4">--}}
+        {{--            <accounting-dashboard-items-chart-component--}}
+        {{--                    type="polarArea"--}}
+        {{--                    title="عمليات">--}}
 
-            </accounting-dashboard-items-chart-component>
-        </div>
-        <div class="col-md-4">
-            <accounting-dashboard-items-chart-component
-                    type="radar"
-                    title="فلاتر">
+        {{--            </accounting-dashboard-items-chart-component>--}}
+        {{--        </div>--}}
+        {{--        <div class="col-md-4">--}}
+        {{--            <accounting-dashboard-items-chart-component--}}
+        {{--                    type="doughnut"--}}
+        {{--                    title="منتجات">--}}
 
-            </accounting-dashboard-items-chart-component>
-        </div>
-    </div>
-{{--    <div class="row">--}}
-{{--        <div class="col-md-4">--}}
-{{--            <accounting-dashboard-items-chart-component--}}
-{{--                    type="polarArea"--}}
-{{--                    title="عمليات">--}}
+        {{--            </accounting-dashboard-items-chart-component>--}}
+        {{--        </div>--}}
+        {{--        <div class="col-md-4">--}}
+        {{--            <accounting-dashboard-items-chart-component--}}
+        {{--                    type="pie"--}}
+        {{--                    title="فلاتر">--}}
 
-{{--            </accounting-dashboard-items-chart-component>--}}
-{{--        </div>--}}
-{{--        <div class="col-md-4">--}}
-{{--            <accounting-dashboard-items-chart-component--}}
-{{--                    type="doughnut"--}}
-{{--                    title="منتجات">--}}
-
-{{--            </accounting-dashboard-items-chart-component>--}}
-{{--        </div>--}}
-{{--        <div class="col-md-4">--}}
-{{--            <accounting-dashboard-items-chart-component--}}
-{{--                    type="pie"--}}
-{{--                    title="فلاتر">--}}
-
-{{--            </accounting-dashboard-items-chart-component>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+        {{--            </accounting-dashboard-items-chart-component>--}}
+        {{--        </div>--}}
+        {{--    </div>--}}
+    @endcan
 @endsection
 
 
