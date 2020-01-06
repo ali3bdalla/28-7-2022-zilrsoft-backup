4fc13cf47a8e33996b1ce2738b0de9a78ac04541
3li3bdalla <ali.dev.sd@gmail.com>
2020-01-06 21:10:34 +0300

    update git

--- app/Attributes/AccountAttributes.php
+++ app/Attributes/AccountAttributes.php
@@ -5,21 +5,12 @@
 	
 	
 	use App\Account;
+	use App\Transaction;
 	
 	trait AccountAttributes
 	{
 
 //
-		public function getCurrentAmount(Account $account)
-		{
-			$currentAmount = $account->debit_transaction()->sum('amount');
-			foreach ($account->children()->get() as $child){
-				$currentAmount += self::getCurrentAmount($child);
-			}
-			
-			return $currentAmount;
-		}
-		
 		public function getLabelAttribute()
 		{
 			return $this->locale_name;
@@ -28,6 +19,28 @@
 		public function getCurrentAmountAttribute()
 		{
 			return money_format("%i",$this->getCurrentAmount($this));
+		}
+		
+		public function getCurrentAmount(Account $account)
+		{
+			
+			if ($account->slug == 'stock'){
+				$currentAmount = Transaction::where('debitable_type','App\Item')->sum('amount') - $currentAmount =
+						Transaction::where('creditable_type','App\Item')->sum('amount');
+			}elseif ($account->type == 'debit'){
+				$currentAmount = $account->debit_transaction()->sum('amount') - $currentAmount =
+						$account->credit_transaction()->sum('amount');
+			}else{
+				$currentAmount = $account->credit_transaction()->sum('amount') - $currentAmount =
+						$account->debit_transaction()->sum('amount');
+			}
+			
+			
+			foreach ($account->children()->get() as $child){
+				$currentAmount += self::getCurrentAmount($child);
+			}
+			
+			return $currentAmount;
 		}
 		
 		public function getTitleAttribute()
