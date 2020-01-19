85af1a35fa4d1baaba4900843f8235f43ac17216
3li3bdalla <ali.dev.sd@gmail.com>
2020-01-19 08:26:02 +0300

    update git

--- app/Http/Controllers/Accounting/HomeController.php
+++ app/Http/Controllers/Accounting/HomeController.php
@@ -19,26 +19,26 @@
 		 */
 		public function index()
 		{
-			$items = Item::all();
-			$prices = [];
-			foreach ($items as $item)
-			{
-				if($item->price_with_tax!=null && $item->price_with_tax>0)
-				{
-					
-					$price = $item->price_with_tax / (1 + ($item->vts/100));
-					$item->update([
-						'price' => $price
-					]);
-					$prices[] =[
-						$item->price_with_tax,
-						$item->price,
-					];
-				}
-				
-			}
-			return  $prices;
-//			return view('accounting.dashboard.index');
+//			$items = Item::all();
+//			$prices = [];
+//			foreach ($items as $item)
+//			{
+//				if($item->price_with_tax!=null && $item->price_with_tax>0)
+//				{
+//
+//					$price = $item->price_with_tax / (1 + ($item->vts/100));
+//					$item->update([
+//						'price' => $price
+//					]);
+//					$prices[] =[
+//						$item->price_with_tax,
+//						$item->price,
+//					];
+//				}
+//
+//			}
+//			return  $prices;
+			return view('accounting.dashboard.index');
 		}
 		
 	}
