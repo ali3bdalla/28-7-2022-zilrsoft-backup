87631d855ff1417aa4c16a0ff5e7e713dd6949fe
3li3bdalla <ali.dev.sd@gmail.com>
2020-01-30 10:46:21 +0300

    update git

--- resources/js/accounting/components/layout/barcodePrinterLayoutComponent.vue
+++ resources/js/accounting/components/layout/barcodePrinterLayoutComponent.vue
@@ -21,7 +21,7 @@
                     <div class="row">
                         <div class="col-md-12 text-right div-col" style="margin-right: 5px;
                         margin-left: -3px;margin-top: -5px;font-family: 'Cairo', sans-serif !important;"
-                             v-text="itemData.ar_name.substr(0,20)">
+                             v-text="itemData.ar_name.substr(0,28)">
 
                         </div>
 
