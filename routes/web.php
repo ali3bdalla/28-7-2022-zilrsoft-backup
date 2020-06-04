f0011e3d3aa6fa13b9ce011149ce336cab6a5583
3li3bdalla <ali.dev.sd@gmail.com>
2020-06-04 14:09:34 +0200

    update git

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,19 @@
 use Illuminate\Support\Facades\DB;
 
 //DB::unprepared(file_get_contents(base_path('database/database.sql')));
+//
+//Route::prefix('limit')->middleware('ProtectLimitMiddleware')->group(function () {
+//    Route::get('items', 'Limit\ItemController@index');
+//    Route::get('items/{item}', 'Limit\ItemController@edit');
+//    Route::post('items/{item}', 'Limit\ItemController@update');
+//    Route::delete('items/{item}/{attachment}', 'Limit\ItemController@delete_attachment');
+//});
 
-Route::prefix('limit')->middleware('ProtectLimitMiddleware')->group(function () {
-    Route::get('items', 'Limit\ItemController@index');
-    Route::get('items/{item}', 'Limit\ItemController@edit');
-    Route::post('items/{item}', 'Limit\ItemController@update');
-    Route::delete('items/{item}/{attachment}', 'Limit\ItemController@delete_attachment');
-});
-
-
-Route::namespace('Web')->group(function () {
-    Route::get('/', 'HomeController@home')->name('home');
-    Route::get('/home', 'HomeController@home')->name('home');
-    Route::get('/{item}', 'ItemController@view')->name('view_item');
-});
+//
+//Route::namespace('Web')->group(function () {
+//    Route::get('/', 'HomeController@home')->name('home');
+//    Route::get('/home', 'HomeController@home')->name('home');
+//    Route::get('/{item}', 'ItemController@view')->name('view_item');
+//});
 
 
