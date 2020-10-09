59a8632203a26c9f5b8133afe73b259672ecc866
aliabdalla <ali.dev.sd@gmail.com>
2020-10-09 07:44:46 +0200

    update

---
+++ storage/framework/views/011eb5f5952749d22a3dc446e73a1316ffa68597.php
@@ -0,0 +1,29 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+<meta name="primary-color" content="#0984e3">
+<meta name="second-color" content="#3498db">
+<meta name="app-locate" content="<?php echo e(app()->getLocale()); ?>">
+<meta name="app-name" content="<?php echo e(config('app.name')); ?>">
+<meta name="app-base-url" content="<?php echo e(route('dashboard.index')); ?>">
+
+
+<meta name="lang-sidebar" content="<?php echo e(json_encode(trans('sidebar'))); ?>">
+<meta name="lang-pagination" content="<?php echo e(json_encode(trans('pagination'))); ?>">
+<meta name="lang-messages" content="<?php echo e(json_encode(trans('messages'))); ?>">
+<meta name="lang-datetime" content="<?php echo e(json_encode(trans('datetime'))); ?>">
+<meta name="lang-table" content="<?php echo e(json_encode(trans('table'))); ?>">
+<meta name="lang-reusable-translator" content="<?php echo e(json_encode(trans('reusable'))); ?>">
+<meta name="lang-validation" content="<?php echo e(json_encode(trans('validation'))); ?>">
+<meta name="lang-items-page" content="<?php echo e(json_encode(trans('pages/items'))); ?>">
+<meta name="lang-invoices-page" content="<?php echo e(json_encode(trans('pages/invoice'))); ?>">
+<meta name="lang-vouchers-page" content="<?php echo e(json_encode(trans('pages/vouchers'))); ?>">
+<meta name="lang-users-page" content="<?php echo e(json_encode(trans('pages/users'))); ?>">
+<meta name="lang-filters-page" content="<?php echo e(json_encode(trans('pages/filters'))); ?>">
+<meta name="lang-branches-page" content="<?php echo e(json_encode(trans('pages/branches'))); ?>">
+<meta name="lang-categories-page" content="<?php echo e(json_encode(trans('pages/categories'))); ?>">
+<meta name="datatableBaseUrl" content="/accounting/datatable/">
+<meta name="BaseApiUrl" content="/accounting/">
+<?php /**PATH /usr/local/var/www/workspace/zilrsoft/resources/views/accounting/layout/head/meta.blade.php ENDPATH**/ ?>
