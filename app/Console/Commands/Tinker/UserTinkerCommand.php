4d12a3452c601dd9e7ca91cc2be896bcc4aba7e4
aliabdalla <ali.dev.sd@gmail.com>
2020-10-15 21:05:22 +0200

    updat

--- app/Console/Commands/Tinker/UserTinkerCommand.php
+++ app/Console/Commands/Tinker/UserTinkerCommand.php
@@ -51,51 +51,51 @@
 //
 			try {
 ////				dd(1);
-//				$invoiceItem = InvoiceItems::find(16201);
-//				$newDiscount = 0;
-//				$currentDiscount = $invoiceItem->discount;
-//				$oldTotal = $invoiceItem->total;
-//				$price = $invoiceItem->price - $currentDiscount;
-//				$total = $price;
-//				$invoiceItem->update(
-//					[
-//						'discount' => $newDiscount,
-//						'price' => $price,
-//						'total' => $price
-//					]
-//				);
-//				dispatch(new UpdateInvoiceBalancesByInvoiceItemsJob($invoiceItem->invoice));
-//
-//
-//				$costTransaction = Transaction::find(59728);//total_credit_amount
-//				$costTransaction->update(
-//					[
-//						'amount' => $costTransaction->amount - $oldTotal + $total,
-//						'total_credit_amount' => $costTransaction->account->total_credit_amount - $oldTotal + $total
-//					]
-//				);
-//
-//				$costTransaction->account->update(
-//					[
-//						'total_credit_amount' => $costTransaction->account->total_credit_amount - $oldTotal + $total
-//					]
-//				);
-//
-//
-//				$costDiscountTransaction = Transaction::find(59729);//total_debit_amount
-//
-//				$costDiscountTransaction->update(
-//					[
-//						'amount' => $costDiscountTransaction->amount - $currentDiscount,
-//						'total_debit_amount' => $costDiscountTransaction->account->total_debit_amount - $currentDiscount
-//					]
-//				);
-//
-//				$costDiscountTransaction->account->update(
-//					[
-//						'total_debit_amount' => $costDiscountTransaction->account->total_debit_amount - $currentDiscount
-//					]
-//				);
+				$invoiceItem = InvoiceItems::find(16201);
+				$newDiscount = 0;
+				$currentDiscount = $invoiceItem->discount;
+				$oldTotal = $invoiceItem->total;
+				$price = $invoiceItem->price - $currentDiscount;
+				$total = $price;
+				$invoiceItem->update(
+					[
+						'discount' => $newDiscount,
+						'price' => $price,
+						'total' => $price
+					]
+				);
+				dispatch(new UpdateInvoiceBalancesByInvoiceItemsJob($invoiceItem->invoice));
+
+
+				$costTransaction = Transaction::find(59728);//total_credit_amount
+				$costTransaction->update(
+					[
+						'amount' => $costTransaction->amount - $oldTotal + $total,
+						'total_credit_amount' => $costTransaction->account->total_credit_amount - $oldTotal + $total
+					]
+				);
+
+				$costTransaction->account->update(
+					[
+						'total_credit_amount' => $costTransaction->account->total_credit_amount - $oldTotal + $total
+					]
+				);
+
+
+				$costDiscountTransaction = Transaction::find(59729);//total_debit_amount
+
+				$costDiscountTransaction->update(
+					[
+						'amount' => $costDiscountTransaction->amount - $currentDiscount,
+						'total_debit_amount' => $costDiscountTransaction->account->total_debit_amount - $currentDiscount
+					]
+				);
+
+				$costDiscountTransaction->account->update(
+					[
+						'total_debit_amount' => $costDiscountTransaction->account->total_debit_amount - $currentDiscount
+					]
+				);
 				
 				$vendors = User::where('is_vendor', true)->get();
 				
