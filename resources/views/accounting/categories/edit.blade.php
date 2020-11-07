7930fd9b611900956d9694759decb295abfbf126
aliabdalla <ali.dev.sd@gmail.com>
2020-11-07 16:46:23 +0300

    add_model_number_to_categories

--- resources/views/accounting/categories/edit.blade.php
+++ resources/views/accounting/categories/edit.blade.php
@@ -47,8 +47,8 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group @error('ar_description')has-error @enderror">
-                        <textarea class="form-control" name="ar_description"
-                                  placeholder="{{trans('pages/categories.ar_description')}}"> {{ empty(old('ar_description')) ?  $category->ar_description :old('ar_description') }}</textarea>
+                        <input class="form-control arabic-input" name="ar_description"
+                                  placeholder="{{trans('pages/categories.ar_description')}}"  value="{{ empty(old('ar_description')) ?  $category->ar_description :old('ar_description') }}"/>
                         @error('ar_description')
                         <small class="text-danger">
                             {{ $message}}
@@ -59,8 +59,9 @@
                 </div>
                 <div class="col-md-6">
                     <div class="form-group @error('description')has-error @enderror">
-                        <textarea class="form-control" name="description" placeholder="{{trans('pages/categories.description')
-                        }}">{{ empty(old('description')) ?  $category->description :old('description') }}</textarea>
+
+                        <input class="form-control arabic-input" name="description" placeholder="{{trans('pages/categories.description')
+                        }}" value="{{ empty(old('description')) ?  $category->description :old('description') }}"/>
                         @error('description')
                         <small class="text-danger">
                             {{ $message}}
