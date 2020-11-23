4de324784466970ca3d1c4d55f306a77cefdad3d
aliabdalla <ali.dev.sd@gmail.com>
2020-11-23 20:53:15 +0300

    update

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -32,8 +32,9 @@
 		
 		public function itemsQtyHolders()
 		{
-			return $this->hasMany(OrderItemQtyHolder::class,'order_id');
+			return $this->hasMany(OrderItemQtyHolder::class, 'order_id');
 		}
+		
 		public function shippingAddress()
 		{
 			return $this->belongsTo(ShippingAddress::class, 'shipping_address_id');
@@ -47,12 +48,12 @@
 		
 		public function generatePayOrderUrl()
 		{
-			return file_get_contents('http://tinyurl.com/api-create.php?url=' . 'https://www.zilrsoft.com/web/orders/'  . $this->id . '/confirm_payment');
+			return file_get_contents('http://tinyurl.com/api-create.php?url=' . url('/web/orders/' . $this->id . '/confirm_payment'));
 		}
 		
 		public function generateCancelOrderUrl()
 		{
-			return file_get_contents('http://tinyurl.com/api-create.php?url=' . 'https://www.zilrsoft.com/web/orders/'  . $this->id . '/cancel');
+			return file_get_contents('http://tinyurl.com/api-create.php?url=' . url('/web/orders/' . $this->id . '/cancel?code=' . $this->cancel_order_code));
 		}
 		
 		
