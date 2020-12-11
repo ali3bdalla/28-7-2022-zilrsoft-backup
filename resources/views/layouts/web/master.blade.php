d18d84655456288e7a4375d38d3a886d08ffc6b2
3li3bdalla <ali.dev.sd@gmail.com>
2020-12-12 02:08:58 +0300

    replace mix to asset

--- resources/views/layouts/web/master.blade.php
+++ resources/views/layouts/web/master.blade.php
@@ -7,8 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title',config('app.name'))</title>
-    <script defer src="{{ mix('js/app_v2.js') }}"></script>
-    <link rel="stylesheet" href="{{ mix('css/tailwind.css') }}">
+    <script defer src="{{ asset('js/app_v2.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     @includeIf("layouts.web.styles")
 </head>
