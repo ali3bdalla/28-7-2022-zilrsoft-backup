4de2f76ff765a495b9769e412c5d9453e3837aa4
3li3bdalla <ali.dev.sd@gmail.com>
2020-12-12 22:17:43 +0300

    fix issue

--- app/Http/Controllers/Accounting/AttachmentController.php
+++ app/Http/Controllers/Accounting/AttachmentController.php
@@ -2,16 +2,13 @@
 
 namespace App\Http\Controllers\Accounting;
 
-use App\Attachment;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Helper\AttachmentsUploaderHelper;
 use Illuminate\Http\Request;
 
 class AttachmentController extends Controller
 {
-    use AttachmentsUploaderHelper;
 
-    public function delete(Attachment $attachment, Request $request)
+    public function delete($attachment, Request $request)
     {
 
     }
