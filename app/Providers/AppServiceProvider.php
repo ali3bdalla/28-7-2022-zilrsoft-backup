00752cfc76bda94e315ca7259b298cb9bb0f0a99
3li3bdalla <ali.dev.sd@gmail.com>
2020-12-13 23:19:42 +0300

    remove telescope from app.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Telescope\TelescopeServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,12 +14,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
 
-        //
-
-        if ($this->app->isLocal()) {
-//            $this->app->register(TelescopeServiceProvider::class);
-            $this->app->register(TelescopeServiceProvider::class);
-        }
 
 
     }
