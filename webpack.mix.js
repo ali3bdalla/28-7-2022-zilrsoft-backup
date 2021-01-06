a6edefd2cf7cde2108a0d29546b9a357c9e6c2e6
ali abdalla <ali.dev.sd@gmail.com>
2021-01-06 13:50:13 +0300

    change css app_v2 to online-store.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss')
 
 mix.js('./resources/js/app.js', 'public/js');
-mix.js('./resources/js/app_v2.js', 'public/js');
+mix.js('./resources/js/online-store.js', 'public/js');
 mix.js('./resources/js/external/app.js', 'public/js/external.js');
 mix.js('./resources/js/upload_images.js', 'public/js');
 mix.sass('./resources/sass/main.scss', 'public/css/main.css').options({
