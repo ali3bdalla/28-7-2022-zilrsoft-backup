1c4b7f52605221bfc9bf8a224010e1d977478fca
ali abdalla <ali.dev.sd@gmail.com>
2021-02-08 17:22:27 +0300

    Auto stash before rebase of "development"

---
+++ database/migrations/2021_02_08_165808_add_is_published_to_items.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Item;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIsPublishedToItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Schema::table('items', function (Blueprint $table) {
+        //     $table->boolean('is_published')->default(false);
+        // });
+        $items = Item::where([['is_kit',false],['is_available_online',true]])->whereHas('attachments')->get();
+        foreach ($items as $key => $item) {
+			$item->update([
+				'is_published' => $item->shouldBeSearchable()
+			]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            //
+        });
+    }
+}
