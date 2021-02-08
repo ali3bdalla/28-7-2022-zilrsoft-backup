e340ef2cce2fecd74a48780e30c3d7765d97936b
ali abdalla <ali.dev.sd@gmail.com>
2021-02-08 17:57:21 +0300

    Auto stash before merge of "master" and "development"

---
+++ resources/js/accounting/components/items/ItemImages.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
