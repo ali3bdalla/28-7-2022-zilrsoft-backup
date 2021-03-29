3a95a0b3053a14dbe2d46547758f2a55e8a6048d
ali abdalla <ali.dev.sd@gmail.com>
2021-03-29 11:29:06 +0200

    fix money format issue

---
+++ app/Helpers/functions.php
@@ -0,0 +1,47 @@
+<?php
+
+if (!function_exists('roundMoney')) {
+
+    function roundMoney($amount)
+    {
+        return round($amount * 10) / 10;
+    }
+}
+
+
+if (!function_exists('moneyFormatter')) {
+    function moneyFormatter($number, $decimal = 2)
+    {
+        return money_format("%i",$number);
+    }
+}
+if (!function_exists('currencyMoneyFormatter')) {
+    function currencyMoneyFormatter($money, $decimal = 2, $currency = "ريال")
+    {
+        return number_format($money, 2, '.', '.') . ' ' . $currency;
+    }
+}
+
+
+if (!function_exists('displayMoney')) {
+    function displayMoney($money, $decimal = 2)
+    {
+        // return $money;
+        return moneyFormatter((float)$money);
+    }
+}
+
+
+if (!function_exists('displayAccountingMoney')) {
+    function displayAccountingMoney($money, $decimal = 2)
+    {
+        $fmt = numfmt_create('en_US', 2);//NumberFormatter::
+        return numfmt_format($fmt, $money, 3);
+    }
+}
+
+
+
+
+
+
