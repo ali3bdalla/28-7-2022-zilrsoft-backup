926ba4d584b58141fd7fb6d53c38c4cd0b2840e2
ali abdalla <ali.dev.sd@gmail.com>
2021-05-02 10:50:00 +0200

    disable google update

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -64,7 +64,7 @@
 				SendPaymentConfirmationListener::class
 			],
             ItemUpdatedEvent::class => [
-                UpdateGoogleRecordListener::class
+//                UpdateGoogleRecordListener::class
             ]
 
 		];
