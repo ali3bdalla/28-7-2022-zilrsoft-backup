c6f789c8bbc4a99f6b19a870e17ecffd282bbd92
ali abdalla <ali.dev.sd@gmail.com>
2021-06-13 20:44:11 +0200

    refactor

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
-	
+
 	namespace App\Providers;
-	
+
 	use App\Http\Views\Composers\ShareLoggedUserComposer;
 	use App\Models\Invoice;
 	use Illuminate\Contracts\Auth\Guard;
@@ -9,7 +9,7 @@
 	use Illuminate\Support\Facades\Blade;
 	use Illuminate\Support\Facades\View;
 	use Illuminate\Support\ServiceProvider;
-	
+
 	class ViewServiceProvider extends ServiceProvider
 	{
 		/**
@@ -19,17 +19,17 @@
 		 */
 		public function register()
 		{
-			
+
 			View::share(
 				'organization_config', [
 					'vts' => 15,
 					'vtp' => 15
 				]
 			);
-			
-			
+
+
 		}
-		
+
 		/**
 		 * Bootstrap services.
 		 *
@@ -38,52 +38,44 @@
 		public function boot(Guard $auth)
 		{
 			//
-			
+
 			Blade::directive(
 				'js_asset', function($file) {
 				$file = str_replace(['(', ')', "'"], '', $file);
-				return '<script src="' . asset($file) . '" ></script>';
+				return '<script src="' . mix($file) . '" ></script>';
 			}
 			);
-			
+
 			Blade::directive(
 				'css_asset', function($file) {
 				$file = str_replace(['(', ')', "'"], '', $file);
-				return '<link href="' . asset($file) . '" rel="stylesheet" />';
+				return '<link href="' . mix($file) . '" rel="stylesheet" />';
 			}
 			);
-			
-			
+
+
 			Blade::directive(
 				'defer_js_asset', function($file) {
 				$file = str_replace(['(', ')', "'"], '', $file);
-				return '<script src="' . asset($file) . '" defer></script>';
+				return '<script src="' . mix($file) . '" defer></script>';
 			}
 			);
-			
-			
+
+
 			Blade::directive(
 				'meta', function($arguments) {
-				
+
 				list($name, $content) = $arguments;
-				
+
 				return '<meta name="' . $name . '" content="' . $content . '">';
 			}
 			);
-			
+
 			view()->composer('*', ShareLoggedUserComposer::class);
 
-//        if(auth()->check())
-//        {
-//        	View::share([
-//        		'loggedManager' => auth()->user()
-//	        ]);
-////	        $reseller = ResellerClosingAccount::where([['is_pending',true],['transaction_type','transfer'],
-////		        ['receiver_id',auth()->user()->id]])->with('creator','receiver')->get()
-//        }
-		
-		
+
+
 		}
-		
-		
+
+
 	}
--- resources/views/accounting/layout/head/css.blade.php
+++ resources/views/accounting/layout/head/css.blade.php
@@ -1,16 +1,16 @@
-{{--<link rel="stylesheet" href="{{ asset('accounting/css/bootstrap.min.css')}}">--}}
-{{--<link rel="stylesheet" href="{{ asset('accounting/css/AdminLTE.min.css')}}">--}}
-{{--<link rel="stylesheet" href="{{ asset('accounting/css/_all-skins.min.css')}}">--}}
-{{--<link href="{{ asset('accounting/css/select2.min.css')}}" rel="stylesheet"/>--}}
-{{--<link rel="stylesheet" href="{{ asset('accounting/css/buttons.css') }}">--}}
+{{--<link rel="stylesheet" href="{{ mix('accounting/css/bootstrap.min.css')}}">--}}
+{{--<link rel="stylesheet" href="{{ mix('accounting/css/AdminLTE.min.css')}}">--}}
+{{--<link rel="stylesheet" href="{{ mix('accounting/css/_all-skins.min.css')}}">--}}
+{{--<link href="{{ mix('accounting/css/select2.min.css')}}" rel="stylesheet"/>--}}
+{{--<link rel="stylesheet" href="{{ mix('accounting/css/buttons.css') }}">--}}
 {{--<link rel="stylesheet" href="{{asset('accounting/css/main.css')}}">--}}
 {{--@if(app()->isLocale('ar'))--}}
-{{--<link rel="stylesheet" href="{{ asset('accounting/css/bootstrap-rtl.css')}}">--}}
-{{--<link rel="stylesheet" href="{{ asset('accounting/css/bootstrap-rt.css') }}">--}}
+{{--<link rel="stylesheet" href="{{ mix('accounting/css/bootstrap-rtl.css')}}">--}}
+{{--<link rel="stylesheet" href="{{ mix('accounting/css/bootstrap-rt.css') }}">--}}
 {{--<link rel="stylesheet"--}}
-{{--      href="{{ asset('accounting/css/AdminLTE-rtl.min.css')}}">--}}
+{{--      href="{{ mix('accounting/css/AdminLTE-rtl.min.css')}}">--}}
 {{--@endif--}}
-{{--<link rel="stylesheet" href="{{ asset('css/rtl.css') }}">--}}
+{{--<link rel="stylesheet" href="{{ mix('css/rtl.css') }}">--}}
 
 
 
