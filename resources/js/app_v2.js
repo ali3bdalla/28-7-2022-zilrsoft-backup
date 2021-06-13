b4bd7d2adf9e87b592b4fa70ba3ab6d593d92327
ali abdalla <ali.dev.sd@gmail.com>
2021-06-13 21:53:36 +0200

    fix bug

---
+++ resources/js/app_v2.js
@@ -0,0 +1,58 @@
+import {InertiaApp} from '@inertiajs/inertia-vue'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VueSimpleAlert from "vue-simple-alert";
+// import {EagleModal} from 'vue-eagle-modal'
+
+import Dialog from 'vue-dialog-loading'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+window.axios = require('axios')
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+let token = document.head.querySelector('meta[name="csrf-token"]')
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content
+} else {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.csrf
+    console.error(
+        'CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token'
+    )
+}
+
+String.prototype.replaceAt = function (index, replacement) {
+    return (
+        this.substr(0, index) +
+        replacement +
+        tAddShippingAddress.vuehis.substr(index + replacement.length)
+    )
+}
+require('./Plugins/plugins')
+
+require('./upload_images');
+
+
+Vue.use(ElementUI)
+Vue.use(Vuex)
+Vue.use(VueSimpleAlert);
+Vue.use(Dialog, {
+    dialogBtnColor: '#0f0',
+    background: 'rgba(0, 0, 0, 0.5)'
+});
+// Vue.use(EagleModal);
+
+
+const store = new Vuex.Store(require('./store/web'))
+Vue.use(InertiaApp)
+
+const app = document.getElementById('app')
+
+new Vue({
+    store: store,
+    render: h =>
+        h(InertiaApp, {
+            props: {
+                initialPage: JSON.parse(app.dataset.page),
+                resolveComponent: name => require(`./Pages/${name}`).default
+            }
+        })
+}).$mount(app)
--- resources/views/layouts/web/master.blade.php
+++ resources/views/layouts/web/master.blade.php
@@ -34,7 +34,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"  integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"  crossorigin="anonymous">
     @endif
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"  type="text/css">
-    @if (app()->isLocale('ar'))
+    <link rel="stylesheet" href="{{ asset('web_assets/template/css/style.min.css') }}" type="text/css">
+
+@if (app()->isLocale('ar'))
     <link rel="stylesheet" href="{{ asset('css/rtl_store.css') }}">
     @else
     <link rel="stylesheet" href="{{ asset('css/store.css') }}">
