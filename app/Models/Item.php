123d3f24e92216dc5e3f62bd919d0357e4be6465
ali abdalla <ali.dev.sd@gmail.com>
2021-06-17 13:46:46 +0200

    disable auto update

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -42,7 +42,7 @@ class Item extends BaseModel
     use SoftDeletes, Searchable;
 
     protected $dispatchesEvents = [
-        'updated' => ItemUpdatedEvent::class,
+//        'updated' => ItemUpdatedEvent::class,
         'created' => ItemCreatedEvent::class
     ];
 
