b7853940b8822d4df747e5ba9f4d44f368c86a7c
ali <info@aliabdalla.dev>
2021-08-21 09:21:42 +0200

    clean up home and frontend controllers

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use App\Models\Category;
 use App\Models\Item;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class HomeController extends Controller
@@ -14,27 +12,14 @@ class HomeController extends Controller
     {
         return redirect(route('web.index', []));
     }
+
     public function index()
     {
-        $offerItem = Item::inRandomOrder()->hasModelNumber()->first();
-        // $modelFilter = $offerItem->filters()->where('filter_id', 38)->first();
-        // if ($modelFilter) {
-        //     if ($modelFilter->value) {
-        //         $offerItem->name = str_replace($modelFilter->value->name, "", $offerItem->name);
-        //         $offerItem->ar_name = str_replace($modelFilter->value->ar_name, "", $offerItem->ar_name);
-        //     }
-        // }
-
-        // return $offerItem->filters()->where('filter_id',38)->first()->value;
         return Inertia::render(
             'Web/Home/Index',
             [
-            // 'products_count' => Item::count(),
-            // 'params' => [],
-            // 'latest' => Item::available()->with('category', 'filters.filter', 'filters.value')->orderBy('created_at', 'desc')->take(15)->get(),
-            'heigest_price' => Item::available()->with('category', 'filters.filter', 'filters.value')->orderBy('online_offer_price', 'desc')->take(15)->get(),
-            'offer_item' => $offerItem
-            // 'items' => Item::with('category', 'filters.filter', 'filters.value')->inRandomOrder()->take(20)->get(),
+                'heigest_price' => Item::available()->with('category', 'filters.filter', 'filters.value')->orderBy('online_offer_price', 'desc')->take(15)->get(),
+                'offer_item' => Item::inRandomOrder()->hasModelNumber()->first(),
             ]
         );
     }
--- app/Http/Middleware/FrontEndMiddleware.php
+++ app/Http/Middleware/FrontEndMiddleware.php
@@ -2,99 +2,85 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
-use Illuminate\Foundation\Http\Exceptions\MaintenanceModeException;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Inertia\Inertia;
 use App\Models\Category;
+use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
-use Algolia\ScoutExtended\Facades\Algolia;
-use App\ItemTag;
-use App\Models\Filter;
-use App\Models\Item;
+use Inertia\Inertia;
 
 class FrontEndMiddleware
 {
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param Request $request
-	 * @param Closure $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        Inertia::setRootView('web');
+        $activeLang = Session::get('webActiveLang', 'ar');
+        $langs = ['ar', 'en'];
+        if ($request->has('web_active_lang') && $request->filled('web_active_lang') && in_array($request->input('web_active_lang'), $langs)) {
+            $activeLang = $request->input('web_active_lang');
+            Session::put('webActiveLang', $activeLang);
+        }
 
-		Inertia::setRootView('web');
+        app()->setlocale($activeLang);
 
-		$activeLang = Session::get('webActiveLang', 'ar');
-		$langs = ['ar', 'en'];
-		if ($request->has('web_active_lang') && $request->filled('web_active_lang') && in_array($request->input('web_active_lang'), $langs)) {
-			$activeLang = $request->input('web_active_lang');
-			Session::put('webActiveLang', $activeLang);
-		}
+        $itemsIndexSearchFilters = config('scout-items-index.attributesForFaceting');
 
-		app()->setlocale($activeLang);
+        $searchFilters = [];
+        foreach ($itemsIndexSearchFilters as $searchFilter) {
+            if (!in_array($searchFilter, [
+                'searchable(online_offer_price)',
+                'searchable(category_name)', 'searchable(category_ar_name)',
+            ])) {
+                $searchFilter = str_replace('searchable(', '', $searchFilter);
+                $searchFilters[] = str_replace(')', '', $searchFilter);
+            }
+        }
 
+        Inertia::share(
+            [
+                'active_logo' => 'ar' == $activeLang ? asset('images/logo_ar.png') : asset('images/logo_en.png'),
+                'active_locale' => app()->getLocale(),
+                'client_logged' => auth('client')->check(),
+                'client' => auth('client')->user(),
+                'app' => config('app'),
+                '$t' => __('store'),
+                'main_categories' => Category::where('parent_id', 0)->get(),
+                'algolia_items_search_as' => 'items_index',
+                'aloglia_daily_search_key' => '3b92b3e1e70e7c12777604f891614933',
+                'algolia_search_filters' => $searchFilters,
+                'algolia_app_key' => 'GM476AOG07',
+                'image_processing_url' => config('services.image_processing.url'),
+                'categories_search_list' => $this->getSearchCategories($request),
+            ]
+        );
 
-		$itemsIndexSearchFilters = config('scout-items-index.attributesForFaceting');
+        return $next($request);
+    }
 
-		$searchFilters = [];
-		foreach ($itemsIndexSearchFilters as $searchFilter) {
-			if (!in_array($searchFilter, [
-				'searchable(online_offer_price)',
-				'searchable(category_name)', 'searchable(category_ar_name)'
-			])) {
-				$searchFilter = str_replace('searchable(', "", $searchFilter);
-				$searchFilters[] = str_replace(')', "", $searchFilter);
-			}
-		}
+    public function getSearchCategories($request)
+    {
+        $result = [];
 
+        if ($request->has('category_id') && $request->filled('category_id') && is_int((int) $request->input('category_id'))) {
+            $category = Category::where('id', $request->input('category_id'))->first();
+            if ($category) {
+                $result[] = $category->locale_name;
+                $list = $category->getChildrenHashMap();
+                if (is_array($list) && '' !== $list[0]) {
+                    $categories = Category::find($list);
+                    foreach ($categories as $key => $sub) {
+                        $result[] = $sub->locale_name;
+                    }
+                }
+            }
+        }
 
-
-
-
-		Inertia::share(
-			[
-				'active_logo' => $activeLang == 'ar' ? asset('images/logo_ar.png') : asset('images/logo_en.png'),
-				'active_locale' => app()->getLocale(),
-				'client_logged' => auth('client')->check(),
-				'client' => auth('client')->user(),
-				"app" => config('app'),
-				'$t' => __("store"),
-				'main_categories' => Category::where('parent_id', 0)->get(),
-				'algolia_items_search_as' => "items_index",
-				'aloglia_daily_search_key' => "3b92b3e1e70e7c12777604f891614933",
-				'algolia_search_filters' => $searchFilters,
-				'algolia_app_key' => "GM476AOG07",
-				'image_processing_url' => config('services.image_processing.url'),
-				'categories_search_list' => $this->getSearchCategories($request),
-			]
-		);
-		return $next($request);
-	}
-
-	public function getSearchCategories($request)
-	{
-		$result  = [];
-
-		if ($request->has('category_id') && $request->filled('category_id') && is_int((int)$request->input('category_id'))) {
-			$category = Category::where('id', $request->input('category_id'))->first();
-			if ($category) {
-				$result[] = $category->locale_name;
-				$list = $category->getChildrenHashMap();
-				if (is_array($list) && $list[0] !== "") {
-					$categories = Category::find($list);
-					foreach ($categories as $key => $sub) {
-						$result[] = $sub->locale_name;
-					}
-				}
-			}
-		}
-
-
-
-		return $result;
-	}
+        return $result;
+    }
 }
