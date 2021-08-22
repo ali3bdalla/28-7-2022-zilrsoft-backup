2561c09eb0d8f54419ad45e91c07340bbded2cad
ali <info@aliabdalla.dev>
2021-08-22 08:59:36 +0200

    fix building store-app/app.js issue

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,14 +2,14 @@ const mix = require('laravel-mix')
 const tailwindcss = require('tailwindcss')
 
 if (mix.inProduction()) {
-  mix.js('./resources/js/store-app/app.js', 'public/js/store-app/app.js')
+  mix.js('resources/js/store-app/app.js', 'public/js/store-app/app.js')
     .sass('resources/sass/store.scss', 'public/css/')
     .sass('resources/sass/rtl_store.scss', 'public/css/')
 } else {
-  mix.js('./resources/js/app.js', 'public/js')
-    .js('./resources/js/store-app/app.js', 'public/js/store-app')
-    .js('./resources/js/external/app.js', 'public/js/external.js')
-    .sass('./resources/sass/main.scss', 'public/css/main.css')
+  mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/store-app/app.js', 'public/js/store-app/app.js')
+    .js('resources/js/external/app.js', 'public/js/external.js')
+    .sass('resources/sass/main.scss', 'public/css/main.css')
     .sass('resources/sass/store.scss', 'public/css/')
     .sass('resources/sass/rtl_store.scss', 'public/css/')
     .browserSync({
