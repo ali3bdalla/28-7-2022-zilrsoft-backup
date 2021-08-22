87177fbcd2a577eea582642185d8f648a5c215ca
ali <info@aliabdalla.dev>
2021-08-22 08:46:45 +0200

    build store-app/app.js file

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require('laravel-mix')
 const tailwindcss = require('tailwindcss')
 
 if (mix.inProduction()) {
-  mix.js('./resources/js/store-app/app.js', 'public/js/store-app')
+  mix.js('./resources/js/store-app/app.js', 'public/js/store-app/app.js')
     .sass('resources/sass/store.scss', 'public/css/')
     .sass('resources/sass/rtl_store.scss', 'public/css/')
 } else {
