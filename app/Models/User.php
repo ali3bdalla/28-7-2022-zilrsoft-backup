0f4e4c376807fd9f1e3245929fe739318fc5fcdf
ali <ali.dev.sd@gmail.com>
2021-09-08 14:36:19 +0200

    clean up

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use App\Models\Traits\Configurable;
 use App\Models\Traits\UserBalanceTrait;
-use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -19,145 +21,145 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class User extends BaseAuthModel
 {
 
-	use SoftDeletes;
-	use Configurable;
-	use UserBalanceTrait;
-	protected $guarded = [];
+    use SoftDeletes;
+    use Configurable;
+    use UserBalanceTrait;
+
+    protected $guarded = [];
 
 
+    protected $appends = [
+        'locale_name',
+        'international_phone_number'
+    ];
+    protected $casts = [
+        'is_vendor' => 'boolean',
+        'is_client' => 'boolean',
+        'is_supplier' => 'boolean',
+        'can_make_credit' => 'boolean',
+        'is_supervisor' => 'boolean',
+        'is_manager' => 'boolean',
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
 
 
-	protected $appends = [
-		'locale_name',
-		'international_phone_number'
-	];
-	protected $casts = [
-		'is_vendor' => 'boolean',
-		'is_client' => 'boolean',
-		'is_supplier' => 'boolean',
-		'can_make_credit' => 'boolean',
-		'is_supervisor' => 'boolean',
-		'is_manager' => 'boolean',
-	];
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class, 'user_id');
+    }
 
-	protected $hidden = [
-		'password'
-	];
+    public function getYearlyBalance(Account $account)
+    {
+        $creditAmount = $account->transactions()->where(
+            [
+                ['type', 'credit'],
+                ['user_id', $this->id]
+            ]
+        )->sum('amount');
 
+        $debitAmount = $account->transactions()->where(
+            [
+                ['type', 'debit'],
+                ['user_id', $this->id]
+            ]
+        )->sum('amount');
 
-	public function orders()
-	{
-		return $this->hasMany(Order::class,'user_id');
-	}
-	public function getYearlyBalance(Account $account)
-	{
-		$creditAmount = $account->transactions()->where(
-			[
-				['type', 'credit'],
-				['user_id', $this->id]
-			]
-		)->sum('amount');
-
-		$debitAmount = $account->transactions()->where(
-			[
-				['type', 'debit'],
-				['user_id', $this->id]
-			]
-		)->sum('amount');
-
-		if ($account->type == 'credit')
-			return $creditAmount - $debitAmount;
+        if ($account->type == 'credit')
+            return $creditAmount - $debitAmount;
 
 
-		return $debitAmount - $creditAmount;
-	}
+        return $debitAmount - $creditAmount;
+    }
 
-	public function invoices()
-	{
-		return $this->hasMany(Invoice::class, 'user_id');
-	}
+    public function invoices(): HasMany
+    {
+        return $this->hasMany(Invoice::class, 'user_id');
+    }
 
-	public function getNameAttribute($value)
-	{
+    public function getNameAttribute($value)
+    {
 
-		return $value;
-		//			return $this->name;
-	}
+        return $value;
+        //			return $this->name;
+    }
 
-	public function getLocaleNameAttribute()
-	{
+    public function getLocaleNameAttribute()
+    {
 
-		return $this->name_ar;
-	}
-
-
-	public function shippingAddresses()
-	{
-		return $this->hasMany(ShippingAddress::class, 'user_id');
-	}
-
-	public function isSystemUser()
-	{
-		return $this->is_system_user;
-	}
-
-	public function _getClientBalance()
-	{
-		return $this->balance;
-	}
-
-	public function _getVendorBalance()
-	{
-		return $this->getOriginal('vendor_balance');
-	}
+        return $this->name_ar;
+    }
 
 
-	public function _isClient()
-	{
-		return $this->is_client == true;
-	}
+    public function shippingAddresses(): HasMany
+    {
+        return $this->hasMany(ShippingAddress::class, 'user_id');
+    }
 
-	public function _isVendor()
-	{
-		return $this->is_vendor == true;
-	}
+    public function isSystemUser()
+    {
+        return $this->is_system_user;
+    }
+
+    public function _getClientBalance()
+    {
+        return $this->balance;
+    }
+
+    public function _getVendorBalance()
+    {
+        return $this->getOriginal('vendor_balance');
+    }
 
 
-	public function _isManager()
-	{
-		return $this->is_manager == true;
-	}
+    public function _isClient(): bool
+    {
+        return $this->is_client == true;
+    }
 
-	public function getInternationalPhoneNumberAttribute()
-	{
+    public function _isVendor(): bool
+    {
+        return $this->is_vendor == true;
+    }
+
+
+    public function _isManager(): bool
+    {
+        return $this->is_manager == true;
+    }
+
+    public function getInternationalPhoneNumberAttribute(): string
+    {
         if (!app()->environment('production'))
             return '+201096295472';
 
-		return '966' . $this->phone_number; // 0966324018
-	}
+        return '966' . $this->phone_number; // 0966324018
+    }
 
-	public function details()
-	{
-		return $this->hasOne(UserDetails::class, 'user_id');
-	}
+    public function details(): HasOne
+    {
+        return $this->hasOne(UserDetails::class, 'user_id');
+    }
 
-	public function organization()
-	{
-		return $this->belongsTo(Organization::class, 'organization_id');
-	}
+    public function organization(): BelongsTo
+    {
+        return $this->belongsTo(Organization::class, 'organization_id');
+    }
 
-	public function creator()
-	{
-		return $this->belongsTo(Manager::class, 'creator_id')->withTrashed();
-	}
+    public function creator()
+    {
+        return $this->belongsTo(Manager::class, 'creator_id')->withTrashed();
+    }
 
-	public function manager()
-	{
-		return $this->hasOne(Manager::class, 'user_id');
-	}
+    public function manager(): HasOne
+    {
+        return $this->hasOne(Manager::class, 'user_id');
+    }
 
-	public function gateways()
-	{
-		return $this->hasMany(UserGateways::class, 'user_id');
-	}
+    public function gateways(): HasMany
+    {
+        return $this->hasMany(UserGateways::class, 'user_id');
+    }
 }
