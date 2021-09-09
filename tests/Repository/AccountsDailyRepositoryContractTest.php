22d9d1a7ae7c08175b2e567152530e562e04be20
ali <info@aliabdalla.dev>
2021-09-10 00:17:29 +0200

    disable ci tests

--- tests/Repository/AccountsDailyRepositoryContractTest.php
+++ tests/Repository/AccountsDailyRepositoryContractTest.php
@@ -12,37 +12,35 @@ class AccountsDailyRepositoryContractTest extends TestCase
 {
     use WithFaker;
 
-    public function testExample()
+
+    private AccountsDailyRepository $accountsDailyRepository;
+
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
-        $this->markTestSkipped();
+        parent::__construct($name, $data, $dataName);
+        $this->accountsDailyRepository = new AccountsDailyRepository();
     }
-//    private AccountsDailyRepository $accountsDailyRepository;
-//    public function __construct(?string $name = null, array $data = [], $dataName = '')
-//    {
-//        parent::__construct($name, $data, $dataName);
-//        $this->accountsDailyRepository = new AccountsDailyRepository();
-//    }
-//
-//    public function testCreateDailyCloseAccountAggregate()
-//    {
-//        $manager = $this->createManager();
-//        $this->actingAs($manager);
-//        $banks = $this->getBanks($manager);
-//        $this->accountsDailyRepository->createDailyCloseAccountAggregate();
-//    }
-//
-//    public function getBanks(Manager $manager)
-//    {
-//        return Account::factory()->state(function (array $attributes) use ($manager) {
-//            return array_merge($attributes, [
-//                'organization_id' => $manager->organization_id,
-//                'type' => 'debit',
-//            ]);
-//        })->count($this->faker->numberBetween(1, 5))->create()->map(function (Account $account) {
-//            return [
-//                'id' => $account->id,
-//                'amount' => $this->faker->randomFloat()
-//            ];
-//        });
-//    }
+
+    public function testCreateDailyCloseAccountAggregate()
+    {
+        $manager = $this->createManager();
+        $this->actingAs($manager);
+        $banks = $this->getBanks($manager);
+        $this->accountsDailyRepository->createDailyCloseAccountAggregate();
+    }
+
+    public function getBanks(Manager $manager)
+    {
+        return Account::factory()->state(function (array $attributes) use ($manager) {
+            return array_merge($attributes, [
+                'organization_id' => $manager->organization_id,
+                'type' => 'debit',
+            ]);
+        })->count($this->faker->numberBetween(1, 5))->create()->map(function (Account $account) {
+            return [
+                'id' => $account->id,
+                'amount' => $this->faker->randomFloat()
+            ];
+        });
+    }
 }
