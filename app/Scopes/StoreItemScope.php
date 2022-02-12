1cf51dfe05548045a9c5127c84e6e9344f08ae13
ali <ali.dev.sd@gmail.com>
2022-02-12 21:37:50 +0200

    chors: fix bugs

--- app/Scopes/StoreItemScope.php
+++ app/Scopes/StoreItemScope.php
@@ -5,6 +5,7 @@ namespace App\Scopes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\Route;
 
 class StoreItemScope implements Scope
 {
@@ -17,16 +18,17 @@ class StoreItemScope implements Scope
      */
     public function apply(Builder $builder, Model $model)
     {
-        if (strpos(url()->current(), 'web') && app()->isProduction()) {
+        if (in_array("ecommerceMiddleware", Route::current()->gatherMiddleware())) {
             $builder
                 ->where([
                     [$builder->qualifyColumn('is_available_online'), true],
                     [$builder->qualifyColumn('available_qty'), '>', 0],
                     [$builder->qualifyColumn('is_kit'), false],
                 ])
-                ->with('category', 'attachments')
                 ->whereHas('category')
-                ->whereHas('attachments')
+                ->whereHas('attachments', function(Builder  $builder) {
+
+                })
                 ->orderBy($builder->qualifyColumn('available_qty'), 'desc');
         }
     }
