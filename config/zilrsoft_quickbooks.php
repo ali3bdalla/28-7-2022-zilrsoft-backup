da7c07a19b8e1363c1559b8e215510bda86fd1e7
ali <ali.dev.sd@gmail.com>
2022-02-26 18:56:18 +0200

    fix quickbooks

--- config/zilrsoft_quickbooks.php
+++ config/zilrsoft_quickbooks.php
@@ -2,9 +2,9 @@
 
 $incomes = 677;
 return [
-    "incomes_account_id" => env('QUICKBOOKS_INCOMES_ACCOUNT_ID', $incomes),
-    "expenses_account_id" => env('QUICKBOOKS_EXPENSES_ACCOUNT_ID', $incomes + 1),
-    "inventory_account_id" => env('QUICKBOOKS_INVENTORY_ACCOUNT_ID', $incomes + 2),
-    "tax_code_account_id" => env('QUICKBOOKS_TAX_CODE_ACCOUNT_ID', "28"),
-    "cash_equivalents_account_id" => env('QUICKBOOKS_CASH_EQUIVALENTS_ACCOUNT_ID', "460"),
+    "incomes_account_id" => env('QUICKBOOKS_INCOMES_ACCOUNT_ID', "677"),
+    "expenses_account_id" => env('QUICKBOOKS_EXPENSES_ACCOUNT_ID', "678"),
+    "inventory_account_id" => env('QUICKBOOKS_INVENTORY_ACCOUNT_ID', "679"),
+    "tax_code_account_id" => env('QUICKBOOKS_TAX_CODE_ACCOUNT_ID', "680"),
+    "cash_equivalents_account_id" => env('QUICKBOOKS_CASH_EQUIVALENTS_ACCOUNT_ID', "624"),
 ];
