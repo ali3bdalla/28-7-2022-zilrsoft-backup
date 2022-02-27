9669df7cfeaaafa7e317c6632357e335de54e9c1
ali <ali.dev.sd@gmail.com>
2022-02-27 10:04:55 +0200

    fix bugs

--- app/Jobs/QuickBooks/SalesQuickBooksSyncJob.php
+++ app/Jobs/QuickBooks/SalesQuickBooksSyncJob.php
@@ -41,68 +41,72 @@ class SalesQuickBooksSyncJob implements ShouldQueue
     public function handle()
     {
 
-        if (!$this->invoice->organization->has_quickbooks || !$this->manager->quickBooksToken || $this->invoice->is_draft) return "UnAuthorized";
-        $quickBooksDataService = app("quickbooksDataService", [
-            "manager" => $this->manager
-        ]);
-        $taxCode = collect(collect($quickBooksDataService->Query("Select * From TaxCode WHERE Active=true"))->offsetGet(0));
-        $salesReceiptLines = $this->invoice->items()->with("item")->whereHas("item", function ($query) {
-            return $query->where('is_kit', false);
-        })->get()->map(function (InvoiceItems $invoiceItems, $index) use ($taxCode) {
-            $data = [
-                "Description" => $invoiceItems->item->locale_name,
-                "DetailType" => "SalesItemLineDetail",
-                "SalesItemLineDetail" => [
-                    "DiscountAmt" => $invoiceItems->discount,
-                    "Qty" => $invoiceItems->qty,
-                    "UnitPrice" => $invoiceItems->price,
-                    "TaxCodeRef" => [
-                        "value" => $taxCode->get("Id")
+        try {
+            if (!$this->invoice->organization->has_quickbooks || !$this->manager->quickBooksToken || $this->invoice->is_draft) return "UnAuthorized";
+            $quickBooksDataService = app("quickbooksDataService", [
+                "manager" => $this->manager
+            ]);
+            $taxCode = collect(collect($quickBooksDataService->Query("Select * From TaxCode WHERE Active=true"))->offsetGet(0));
+            $salesReceiptLines = $this->invoice->items()->with("item")->whereHas("item", function ($query) {
+                return $query->where('is_kit', false);
+            })->get()->map(function (InvoiceItems $invoiceItems, $index) use ($taxCode) {
+                $data = [
+                    "Description" => $invoiceItems->item->locale_name,
+                    "DetailType" => "SalesItemLineDetail",
+                    "SalesItemLineDetail" => [
+                        "DiscountAmt" => $invoiceItems->discount,
+                        "Qty" => $invoiceItems->qty,
+                        "UnitPrice" => $invoiceItems->price,
+                        "TaxCodeRef" => [
+                            "value" => $taxCode->get("Id")
+                        ],
                     ],
-                ],
-                "Id" => $invoiceItems->id,
-                "LineNum" => ($index + 1),
-                "Amount" => $invoiceItems->total,
-            ];
-
-            if ($invoiceItems->item->quickbooks_id) {
-                $data["SalesItemLineDetail"]["ItemRef"] = [
-                    "name" => $invoiceItems->item->locale_name,
-                    "value" => $invoiceItems->item->quickbooks_id
+                    "Id" => $invoiceItems->id,
+                    "LineNum" => ($index + 1),
+                    "Amount" => $invoiceItems->total,
                 ];
 
-            }
-            return $data;
-        })->toArray();
-        $data = [
-            "ApplyTaxAfterDiscount" => true,
-            "DocNumber" => $this->invoice->invoice_number,
-            "TotalAmt" => $this->invoice->subtotal,
-            "TxnDate" => Carbon::parse($this->invoice->created_at)->toDateString(),
-            "DepositToAccountRef" => [
-                "value" => config('zilrsoft_quickbooks.cash_equivalents_account_id')
-            ],
-            "PaymentRefNum" => "#" . $this->invoice->invoice_number,
-            "Line" => $salesReceiptLines
-        ];
+                if ($invoiceItems->item->quickbooks_id) {
+                    $data["SalesItemLineDetail"]["ItemRef"] = [
+                        "name" => $invoiceItems->item->locale_name,
+                        "value" => $invoiceItems->item->quickbooks_id
+                    ];
+
+                }
+                return $data;
+            })->toArray();
+            $data = [
+                "ApplyTaxAfterDiscount" => true,
+                "DocNumber" => $this->invoice->invoice_number,
+                "TotalAmt" => $this->invoice->subtotal,
+                "TxnDate" => Carbon::parse($this->invoice->created_at)->toDateString(),
+                "DepositToAccountRef" => [
+                    "value" => config('zilrsoft_quickbooks.cash_equivalents_account_id')
+                ],
+                "PaymentRefNum" => "#" . $this->invoice->invoice_number,
+                "Line" => $salesReceiptLines
+            ];
 //        if ($this->manager->quickbooks_class_id) {
 //            $data["ClassRef"] = [
 //                "value" => "{$this->manager->quickbooks_class_id}"
 //            ];
 //        }
-        if ($this->invoice->user->quickbooks_customer_id) {
-            $data["CustomerRef"] = [
-                "value" => "{$this->invoice->user->quickbooks_customer_id}"
-            ];
-        }
-        $salesReceipt = SalesReceipt::create(
-            $data
-        );
-        $createdQuickBooksInvoice = $quickBooksDataService->Add($salesReceipt);
-        if ($createdQuickBooksInvoice) {
-            $this->invoice->update([
-                'quickbooks_id' => $createdQuickBooksInvoice->Id
-            ]);
+//            if ($this->invoice->user->quickbooks_customer_id) {
+//                $data["CustomerRef"] = [
+//                    "value" => $this->invoice->user->quickbooks_customer_id
+//                ];
+//            }
+            $salesReceipt = SalesReceipt::create(
+                $data
+            );
+            $createdQuickBooksInvoice = $quickBooksDataService->Add($salesReceipt);
+            if ($createdQuickBooksInvoice) {
+                $this->invoice->update([
+                    'quickbooks_id' => $createdQuickBooksInvoice->Id
+                ]);
+            }
+        } catch (Exception $exception) {
+            throw new $exception;
         }
     }
 }
