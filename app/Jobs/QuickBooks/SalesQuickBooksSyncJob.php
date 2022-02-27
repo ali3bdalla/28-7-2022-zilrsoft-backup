35786f97062f91f72fd1b1014f703260273f35f6
ali <ali.dev.sd@gmail.com>
2022-02-27 09:58:16 +0200

    disable classes

--- app/Jobs/QuickBooks/SalesQuickBooksSyncJob.php
+++ app/Jobs/QuickBooks/SalesQuickBooksSyncJob.php
@@ -85,11 +85,11 @@ class SalesQuickBooksSyncJob implements ShouldQueue
             "PaymentRefNum" => "#" . $this->invoice->invoice_number,
             "Line" => $salesReceiptLines
         ];
-        if ($this->manager->quickbooks_class_id) {
-            $data["ClassRef"] = [
-                "value" => "{$this->manager->quickbooks_class_id}"
-            ];
-        }
+//        if ($this->manager->quickbooks_class_id) {
+//            $data["ClassRef"] = [
+//                "value" => "{$this->manager->quickbooks_class_id}"
+//            ];
+//        }
         if ($this->invoice->user->quickbooks_customer_id) {
             $data["CustomerRef"] = [
                 "value" => "{$this->invoice->user->quickbooks_customer_id}"
