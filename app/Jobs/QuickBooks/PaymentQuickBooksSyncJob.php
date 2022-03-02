f6253e1986960c1afddc4c1e25292b5a76f1acc5
ali <ali.dev.sd@gmail.com>
2022-03-02 21:45:05 +0200

    add sales voucher

---
+++ app/Jobs/QuickBooks/PaymentQuickBooksSyncJob.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Jobs\QuickBooks;
+
+use App\Enums\VoucherTypeEnum;
+use App\Models\Manager;
+use App\Models\Voucher;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use QuickBooksOnline\API\Facades\Payment;
+
+class PaymentQuickBooksSyncJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private Voucher $voucher;
+    private Manager $manager;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Voucher $voucher, Manager $manager)
+    {
+        $this->voucher = $voucher;
+        $this->manager = $manager;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function handle()
+    {
+
+        if ($this->voucher->quickbooks_id != null || !$this->voucher->payment_type->equals(VoucherTypeEnum::receipt()) || !$this->voucher->organization->has_quickbooks || !$this->manager->quickBooksToken || $this->voucher->is_draft) return "UnAuthorized";
+        $quickBooksDataService = app("quickbooksDataService", [
+            "manager" => $this->manager
+        ]);
+
+        $data = [
+            "ClassRef" => [
+                "value" => $this->manager->quickbooks_class_id
+            ],
+            "TotalAmt" => $this->voucher->amount,
+            "TxnDate" => Carbon::parse($this->voucher->created_at)->toDateString(),
+            "MetaData" => [
+                "CreateTime" => $this->voucher->created_at,
+                "LastUpdatedTime" => $this->voucher->updated_at
+            ],
+            "DepositToAccountRef" => [
+                "value" => config('zilrsoft_quickbooks.cash_equivalents_account_id')
+            ],
+            "CustomerRef" => [
+                "value" => $this->voucher->user->quickbooks_customer_id
+            ]
+        ];
+
+        $bill = Payment::create(
+            $data
+        );
+
+        $created = $quickBooksDataService->Add($bill);
+        if ($created) {
+            $this->voucher->update([
+                'quickbooks_id' => $created->Id
+            ]);
+            return;
+        }
+
+        $error = $quickBooksDataService->getLastError();
+        if ($error) {
+            throw  new Exception(json_encode([
+                $error->getIntuitErrorMessage(),
+                $error->getIntuitErrorDetail(),
+                $error->getIntuitErrorElement(),
+                $error->getIntuitErrorCode(),
+                $this->voucher->toArray()
+            ]));
+        }
+
+    }
+}
--- app/Repository/Eloquent/VoucherRepository.php
+++ app/Repository/Eloquent/VoucherRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository\Eloquent;
 
 use App\Dto\VoucherDto;
 use App\Enums\VoucherTypeEnum;
+use App\Jobs\QuickBooks\PaymentQuickBooksSyncJob;
 use App\Models\Voucher;
 use App\Repository\EntryRepositoryContract;
 use App\Repository\VoucherRepositoryContract;
@@ -43,8 +44,14 @@ class VoucherRepository extends BaseRepository implements VoucherRepositoryContr
     {
         return DB::transaction(function () use ($voucherDto) {
             $voucher = Voucher::factory()->setDto($voucherDto)->create();
-            if ($voucher->payment_type->equals(VoucherTypeEnum::receipt())) $this->entryRepositoryContract->registerClientVoucherEntry($voucher, $voucher->account);
-            else  $this->entryRepositoryContract->registerVendorVoucherEntry($voucher, $voucher->account);
+            if ($voucher->payment_type->equals(VoucherTypeEnum::receipt())) {
+                $this->entryRepositoryContract->registerClientVoucherEntry($voucher, $voucher->account);
+                dispatch(new PaymentQuickBooksSyncJob($voucher));
+            }
+            else {
+                $this->entryRepositoryContract->registerVendorVoucherEntry($voucher, $voucher->account);
+//                dispatch(new PaymentQuickBooksSyncJob($voucher));
+            }
             return $voucher;
         });
     }
