b107f1f6269482c26b036e2c42ef584901d74716
ali abdalla <ali.dev.sd@gmail.com>
2022-07-11 23:21:12 +0200

    wip

---
+++ app/Enums/InvoiceItemStatusEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+use Spatie\Enum\Laravel\Enum;
+/**
+ * @method static self invoice_status()
+ * @method static self received()
+ * @method static self returned()
+ * @method static self sent_to_vendor()
+ */
+class InvoiceItemStatusEnum extends Enum
+{
+
+}
